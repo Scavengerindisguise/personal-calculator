b774896d5f2aff90a4f73616719ae57ac35bfdde
unknown <sam.3337@gmail.com>
2020-06-21 14:10:04 +0530

    Version 1

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -5,8 +5,24 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            income: [
+                { source: '', amount: '', date: '' }
+            ]
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.addIncomeRow = this.addIncomeRow.bind(this);
+    }
+
+    handleChange(event) {
+        const name = event.target.name;
+        this.setState({
+            [name]: event.target.value
+        });
+    }
+
+    addIncomeRow(event) {
+        alert(JSON.stringify(this.state));
+        event.preventDefault();
     }
 
     render() {
@@ -16,29 +32,30 @@ class Dashboard extends React.Component {
                 <div className='row text-center'>
                     <div className="col-md-6">
                         <h4>Income</h4>
-                        <Form>
+                        
+                        <Form onSubmit={this.addIncomeRow}>
                             <Row className="justify-content-center" form>
                                 <Col md={3}>
                                     <FormGroup>
                                         <Label for="source">Source</Label>
-                                        <Input bsSize="sm" type="text" name="source" id="source" placeholder="Source"/>
+                                        <Input bsSize="sm" onChange={this.handleChange} value={this.state.source} type="text" name="source" id="source" placeholder="Source" />
                                     </FormGroup>
                                 </Col>
                                 <Col md={3}>
                                     <FormGroup>
                                         <Label for="amount">Amount</Label>
-                                        <Input bsSize="sm" type="number" name="amount" id="amount" placeholder="Amount"/>
+                                        <Input bsSize="sm" onChange={this.handleChange} value={this.state.amount} type="number" name="amount" id="amount" placeholder="Amount" />
                                     </FormGroup>
                                 </Col>
                                 <Col md={3}>
                                     <FormGroup>
                                         <Label for="date">Date</Label>
-                                        <Input bsSize="sm" type="date" name="date" id="date" placeholder="Date"/>
+                                        <Input bsSize="sm" onChange={this.handleChange} value={this.state.date} type="date" name="date" id="date" placeholder="Date" />
                                     </FormGroup>
                                 </Col>
                             </Row>
                             <Row className="justify-content-end">
-                            <Button outline size="sm" color="info">Add</Button>
+                                <Button type="submit" value="submit" outline size="sm" color="info">Add</Button>
                             </Row>
                         </Form>
                     </div>
